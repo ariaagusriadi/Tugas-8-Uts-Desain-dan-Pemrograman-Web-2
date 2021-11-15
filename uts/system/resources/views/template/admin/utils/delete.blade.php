4e35073c151af0ac186bac4c705777787506992f
Aria Agus Riadi <ariaagusriadi100@gmail.com>
2021-11-15 19:31:25 +0700

    a

--- uts/system/resources/views/template/admin/utils/delete.blade.php
+++
@@ -1,6 +0,0 @@
-<form action="{{ $url }}" class="form-inline" method="POST"
-    onsubmit="return confirm('are you sure delete item?')">
-    @csrf
-    @method("delete")
-    <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
-</form>
